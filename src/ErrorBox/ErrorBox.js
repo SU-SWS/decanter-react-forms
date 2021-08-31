09dfe1a356b6275ce07d7c7274becc5285bf8413
rebeccahongsf <rebeccahongsf@gmail.com>
2021-08-31 08:25:23 -0700

    ADAPT-3128 Add list example and adjust icon sizing

--- src/ErrorBox/ErrorBox.js
+++ src/ErrorBox/ErrorBox.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { dcnb } from "cnbuilder";
-import { InformationCircleIcon } from "@heroicons/react/solid";
+import { ExclamationCircleIcon } from "@heroicons/react/solid";
 import { DismissButton } from "../DismissButton/DismissButton";
 
 /**
@@ -21,14 +21,21 @@ export const ErrorBox = ({ className, children, ...props }) => {
   return (
     <div
       className={dcnb(
-        "su-bg-digital-red-light su-px-18 su-pt-18 su-pb-26 su-flex su-flex-row su-items-start su-w-fit",
+        "su-bg-digital-red-light su-bg-opacity-20 su-px-18 su-pt-18 su-pb-26 su-flex su-flex-row su-items-start su-w-fit",
         className
       )}
     >
-      <InformationCircleIcon className="su-w-20 su-mr-6" aria-hidden="true" />
-      <div className="su-flex-grow su-max-w-400">{children}</div>
+      <div>
+        <ExclamationCircleIcon
+          className="su-w-20 su-mr-6 su-text-digital-red"
+          aria-hidden="true"
+        />
+      </div>
+      <div className="su-flex-grow su-max-w-400 su-text-digital-red">
+        {children}
+      </div>
       <DismissButton
-        className="su-ml-18"
+        className="su-ml-18 su-text-digital-red"
         onClick={() => {
           setDismissed(true);
         }}
--- src/ErrorBox/ErrorBox.stories.js
+++ src/ErrorBox/ErrorBox.stories.js
@@ -10,6 +10,32 @@ const errorBody = (
   </p>
 );
 
+const errorListBody = (
+  <div className="last:su-mb-0 su-leading-display su-card-paragraph">
+    <p className="su-mb-6">
+      <strong>Please revise inputs to the following fields.</strong>
+    </p>
+    <p className="su-mb-18">
+      Some of your inputs need revising. Please see each field for more
+      information.
+    </p>
+    <ul className="su-list-none su-pl-0">
+      <li className="su-mb-12">
+        <a href="/">Address line 1</a>
+      </li>
+      <li className="su-mb-12">
+        <a href="/">Expiration date</a>
+      </li>
+      <li className="su-mb-12">
+        <a href="/">Field name one</a>
+      </li>
+      <li className="su-mb-12">
+        <a href="/">Field name two</a>
+      </li>
+    </ul>
+  </div>
+);
+
 export default {
   title: "Composite/Error Box",
   component: ErrorBox,
@@ -21,4 +47,9 @@ const ErrorBoxTemplate = ({ ...rest }) => <ErrorBox {...rest} />;
 export const Default = ErrorBoxTemplate.bind({});
 Default.args = {
   children: errorBody,
+};
+
+export const List = ErrorBoxTemplate.bind({});
+List.args = {
+  children: errorListBody,
 };
